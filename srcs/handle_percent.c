748a621b9900c002f9def092d8cc462f0f664eb3
mohamedbouzaien <mohamed.bouzaien@esprit.tn>
2020-05-16 03:09:51 +0200

    %i first test

--- srcs/handle_percent.c
+++ srcs/handle_percent.c
@@ -6,7 +6,7 @@
 /*   By: mbouzaie <mbouzaie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/26 15:48:54 by mbouzaie          #+#    #+#             */
-/*   Updated: 2020/05/13 03:13:42 by mbouzaie         ###   ########.fr       */
+/*   Updated: 2020/05/16 03:08:42 by mbouzaie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int		find(char *tab, char element)
 	return (-1);
 }
 
-void	fill_va_fncts(int (*print_va_fncts[7])(va_list *, t_flag flag))
+void	fill_va_fncts(int (*print_va_fncts[8])(va_list *, t_flag flag))
 {
 	print_va_fncts[0] = print_va_str;
 	print_va_fncts[1] = print_va_char;
@@ -35,17 +35,18 @@ void	fill_va_fncts(int (*print_va_fncts[7])(va_list *, t_flag flag))
 	print_va_fncts[4] = print_va_hex;
 	print_va_fncts[5] = print_va_hex;
 	print_va_fncts[6] = print_va_udec;
+	print_va_fncts[7] = print_va_nbr;
 }
 
 void	handle_percent(const char *format, int *pos, int *psize, va_list *ap)
 {
 	int		tmp;
-	int		(*print_va_fncts[7])(va_list *, t_flag flag);
+	int		(*print_va_fncts[8])(va_list *, t_flag flag);
 	char	*tab;
 	t_flag	flag;
 
 	fill_va_fncts(print_va_fncts);
-	tab = (char[8]) {'s', 'c', 'd', 'p', 'x', 'X', 'u', 0};
+	tab = (char[9]) {'s', 'c', 'd', 'p', 'x', 'X', 'u', 'i', 0};
 	*psize += *pos;
 	flag = fill_flag_data(format, pos, ap);
 	*psize -= *pos;
